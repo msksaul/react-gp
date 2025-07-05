bd510ad5cc5fd98d3ad810c36f1ffb46dbc6f5f1
Melvin Saúl <msksaul@gmail.com>
2025-07-05 11:47:22 -0600

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
-import { BookReader, FocusInput } from './components'
+import { ShoppingCart } from './components'
 
 function App() {
 
   return (
     <>
-      <BookReader />
-      <FocusInput />
+      <ShoppingCart />
     </>
   )
 }
